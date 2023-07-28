4cde1b1a164b36849c77875c2c77d041d7c626a6
= <=>
2023-07-27 20:41:47 -0400

    express first version

---
+++ always_music.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE always_music;
+
+\c always_music
+
+CREATE TABLE student (
+    rut VARCHAR(12),
+    nombre VARCHAR(200) NOT NULL,
+    curso VARCHAR(10) NOT NULL,
+    nivel VARCHAR(10) NOT NULL,
+    PRIMARY KEY (rut)
+);
+
+INSERT INTO student(rut,nombre,curso,nivel) VALUES ('15.414.390-4','Sixto Guerra','JS27','VESPERTINO');
