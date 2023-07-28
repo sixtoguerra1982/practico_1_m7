7473f758b7ae633f5614633a7c637a366cdc6c03
= <=>
2023-07-27 21:21:26 -0400

    install dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+require('dotenv').config()
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
