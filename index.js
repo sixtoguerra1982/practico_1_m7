02eb0fc6cad92759feb3cb1a3b1ca00e837adda3
DaisyCastilloS <daisyktbpa@gmail.com>
2023-07-28 20:31:33 -0400

    busqueda por rut

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { allStudents} = require('./model/student');
+const { allStudents, searchStudent } = require("./model/student");
 
-require('dotenv').config()
+require("dotenv").config();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+app.get("/allstudents", async (req, res) => {
+  // CLIENT DE CONEXION
 
-app.get('/allstudents', async (req, res) => {
-    // CLIENT DE CONEXION
-    
-    try {
-        const students = await allStudents();
-        res.json(students.rows);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-  });
+  try {
+    const students = await allStudents();
+    res.json(students.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/search/:rut", async (req, res) => {
+  let rut = req.params.rut;
+  try {
+    const student = await searchStudent(rut);
+    res.json(student.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
--- model/student.js
+++ model/student.js
@@ -1,21 +1,43 @@
-const { pool } = require('../utils/conexion');
+const { pool } = require("../utils/conexion");
 
 const allStudents = async () => {
-    try {
-        // LISTAR TODOS LOS ESTUDIANTES
-        //  *1) conectarme a la BD;
-        const client = await pool.connect();
-        const students = await client.query('SELECT rut, nombre, curso, nivel FROM student');
-        console.log('students:', students);
-        client.release();
-        return students
-    } catch (err) {
-        console.log('err.message:', err.message);
-        console.log('err.stack:', err.stack);
-        client.release();
-        return [];
+  try {
+    // LISTAR TODOS LOS ESTUDIANTES
+    //  *1) conectarme a la BD;
+    const query = { text: "SELECT rut, nombre, curso, nivel FROM student" };
+    const client = await pool.connect();
+    const students = await client.query(query);
+    console.log("students:", students);
+    client.release();
+    return students;
+  } catch (err) {
+    console.log("err.message:", err.message);
+    console.log("err.stack:", err.stack);
+    client.release();
+    return [];
+  }
+};
+
+const searchStudent = async (rut) => {
+  try {
+    // BUACAR ESTUDIANTE POR RUT
+    //  *1) conectarme a la BD;
+    const query = {
+      text: "SELECT rut, nombre, curso, nivel FROM student WHERE rut= $1",
+      values: [rut],
+    };
+    const result = await pool.query(query);
+    console.log(`Busqueda de estudiante: ${rut}`, result);
+    if (result.length > 0) {
+      return result;
+    } else {
+      return [{ message: `Busqueda de estudiante: ${rut} ,no encontrado` }];
     }
-}
+  } catch (err) {
+    console.log("err.message:", err.message);
+    console.log("err.stack:", err.stack);
+    return [];
+  }
+};
 
-
-module.exports = { allStudents }
+module.exports = { allStudents, searchStudent };
